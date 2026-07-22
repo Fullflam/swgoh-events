39f147ee5bd58e9619a977e1ac42978450a98d7d
GitHub Action <action@github.com>
2026-07-22 14:14:21 +0000

    Update vaisseau stars [auto]

--- vaisseau_stars.py
+++ vaisseau_stars.py
@@ -1,2 +1,252 @@
 VAISSEAU_STARS = {
+    "28fC4UwHTxCuaawF-d_31w": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "Uk2EnOhNQn2uQK1HueWrKA": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "nnjbqeBjS6CV6rGPQg_kHA": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "CJBjorbIQ8eypeyLV3vWQQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "2kgvI3coQF62BZLA2RQnKQ": {
+        "executor": 4,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "3S1ns8dES92r2sphoXoYxw": {
+        "executor": 5,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "CtnzovAMT6Cuyt2-FR4mVQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "B8xyFbeaTfe6Z4y7Ete0Uw": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "puXdDmhxQ8a-_wZ4M_b0GA": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "L0glG1F_QHar63kbAYVAXQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "DT-33ly0SH62IJqgUp9VkQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "-6DxXhM5T2e2qiGSFL6O6A": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "t0WcA0V5RD--5kunanx73g": {
+        "executor": 7,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "5BlaKwiXR7CVZqPn5DgQLA": {
+        "executor": 4,
+        "profundity": 4,
+        "leviathan": 0
+    },
+    "WQ2fAeRtQbSh_y9p9p0cJg": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "VRTuEdD_SjKI9WsacHEv2A": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "KZAwXvNYTsyzOPuX_bpWtQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "y1vaRr2TTMuiSqzKzK9Lmg": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "a1unQc3rTvuHBRNIp2RTZQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "H5mkcRL_Su6ycVEz5BG67A": {
+        "executor": 6,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "nK8AS3mLToKpGTUsXScFOA": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "KI4gLpCfQ9q8SQnU55WP2w": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "ZBg-Na2MR3-Xn1-LF5zucA": {
+        "executor": 4,
+        "profundity": 4,
+        "leviathan": 0
+    },
+    "zLzGs7-4S5GXP3AXY_Cbmg": {
+        "executor": 5,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "ho3B-JlvSbeVZA_fgJ37Rg": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "SHzdSCHVQtav2Y3ew64JUw": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "gw3a2aR_TAuS0WHiA-5Gkg": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "JtTVQUm_Qqy33v2i3meBXg": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "KmBcP-LUQLOZT_jlhBpouA": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "D-HM6G3YShajJttOHOiOWA": {
+        "executor": 4,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "n24LULsjSMK3mv3LzaYhsQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "ZZTbZRsqTZyj1NlfOqjULw": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "iJQ8-KyITd-HPimuIuOalg": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "aTEpzTRMRzm8EWUPJwxSjA": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "y81xWRVcQ3iwTg9kCkXQlQ": {
+        "executor": 7,
+        "profundity": 7,
+        "leviathan": 0
+    },
+    "IYjaoG4vQpGjg51gsndsYw": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "Ls22hqWuStqUCB1U20QjSA": {
+        "executor": 6,
+        "profundity": 5,
+        "leviathan": 4
+    },
+    "hrCmFEEyR6qc3j9ZzGRrDA": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "I2U0gTz8RwKE8lSBYUvN-A": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "eJQPvshYTvyHao-tlm16Lg": {
+        "executor": 4,
+        "profundity": 4,
+        "leviathan": 0
+    },
+    "q4bzf0fuS6G26Jys0mY_pQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "Q7Sc7ll9QJ2G7s4NT008eQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "qakpEXZjSkS0lQVRAdNwgQ": {
+        "executor": 5,
+        "profundity": 5,
+        "leviathan": 0
+    },
+    "o31HcPKVT5SYuh349sf2ug": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "hoXZjqEUQGWhzfWbI5O4dQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "zHJ2bzxQR-Gna6Kc2TTvJQ": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "H3ke5xLjQGODfRMwsrhBuw": {
+        "executor": 5,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "Nb1V6DlMSLqUPMTAg8yRlQ": {
+        "executor": 7,
+        "profundity": 0,
+        "leviathan": 5
+    },
+    "Mq7wOuh2Ri6uc8-OnwGY-g": {
+        "executor": 0,
+        "profundity": 0,
+        "leviathan": 0
+    },
+    "zSgV7yOSRjGZ4C-fn6UDLQ": {
+        "executor": 5,
+        "profundity": 0,
+        "leviathan": 0
+    }
 }
